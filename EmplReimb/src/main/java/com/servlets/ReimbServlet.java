05390cce9c14386be450162c7ff747b6adca9ac1
kirsalyheredia <kirsaly.heredia@gmail.com>
2020-02-26 10:03:00 -0500

    updated reimbServlet to read angular data

--- EmplReimb/src/main/java/com/servlets/ReimbServlet.java
+++ EmplReimb/src/main/java/com/servlets/ReimbServlet.java
@@ -7,24 +7,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.models.Reimbursement;
 import com.service.ReimbService;
 
 public class ReimbServlet extends HttpServlet{
 
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet initializing");
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		resp.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+		resp.addHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE");
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		
+		super.service(req, resp);
+	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		int amount = Integer.parseInt(req.getParameter("amount"));
-		String type = req.getParameter("type");
-		String description = req.getParameter("description");
-		String receipt = req.getParameter("receipt");
-				
-		//passing to service layer for validation before inputing to the db
-		ReimbService.validateReimb(amount, type, description, receipt);
+		ObjectMapper om = new ObjectMapper();
+		Reimbursement reimb = om.readValue(req.getReader(), Reimbursement.class);
+						
+		System.out.println(reimb); //check
 		
+		//passing to service layer for validation before inputing to the db
+		ReimbService.validateReimb(reimb);
 		resp.setStatus(201); // created
 	}
-	
 	
 }
--- EmplReimb/src/main/java/com/util/ConnectionUtil.java
+++ EmplReimb/src/main/java/com/util/ConnectionUtil.java
@@ -7,20 +7,20 @@ import java.sql.SQLException;
 
 public class ConnectionUtil {
 
-public static Connection getConnection() {
-		
-	//environment variables to connect to database
-	String url = System.getenv("JDBC_URL");
-	String uname = System.getenv("uname");
-	String pword = System.getenv("pword");
-
-	try {
-		System.out.println("Trying to connect to the database...");
-		return DriverManager.getConnection(url, uname, pword);
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}
-	return null;
+	public static Connection getConnection() {
+			
+		//environment variables to connect to database
+		String url = System.getenv("JDBC_URL");
+		String uname = System.getenv("uname");
+		String pword = System.getenv("pword");
+	
+		try {
+			System.out.println("Trying to connect to the database...");
+			return DriverManager.getConnection(url, uname, pword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 }
