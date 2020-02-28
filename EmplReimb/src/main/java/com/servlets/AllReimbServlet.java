2c6c3c59f1b5b432d92455429a8d222c9e4d4010
Kirsaly Heredia <60357183+kirsalyHeredia@users.noreply.github.com>
2020-02-27 23:17:06 -0500

    fixed allReimb dao and servlet

--- EmplReimb/src/main/java/com/servlets/AllReimbServlet.java
+++ EmplReimb/src/main/java/com/servlets/AllReimbServlet.java
@@ -18,8 +18,17 @@ public class AllReimbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	ObjectMapper om = new ObjectMapper();
 
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+		resp.addHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE");
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		
+		super.service(req, resp);
+	}
+	
    	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+   		
    		if(req.getPathInfo() == null) {
 			resp.setStatus(404);
 			return;
